e9b9bdf10f532ae219ca4d3eccc42b898d468d2c
tatyana.zhukova <tatyana.zhukova@sytecs.com.ua>
2021-08-09 13:03:01 +0300

    project apps

---
+++ js/global.js
@@ -0,0 +1,167 @@
+const enConst = {
+  'header-home': 'Home',
+  'header-works': 'How it works',
+  'header-special': 'Special',
+  'header-pricing': 'Pricing',
+  'header-review': 'Review',
+  'header-contact': 'Contact',
+
+  'maincont-service': 'Service for rent applications',
+  'maincont-created': 'Created by TataRozhdez Company',
+  'maincont-more': 'Learn more',
+
+  'howworks-title': 'How it Works?',
+
+  'testimonials': 'Testimonials',
+  'testimonials-first-txt': '“ COOL - is one of the most popular services. For easy viewing products are divided into groups: directions, categories, type of purchase, type of platform. I mostly used the global type of purchase. ”',
+  'testimonials-ft-name': 'Ekaterina',
+  'testimonials-ft-spec': 'SEO-specialist',
+
+  'testimonials-second-txt': '“ I’ve been using it for half year and I haven’t found any flaws yet. It’s very convenient and informative. I recommend all friends, relatives, acquaintances. ”',
+  'testimonials-sct-name': 'Roman',
+  'testimonials-sct-spec': 'Freelancer',
+
+  'testimonials-third-txt': '“ The app was recommended by friends and I thank them. TataRozhdez - it is simple and easy to understand. The service is fast, without any problems. After the start of the lease of the application it is possible to generate all possible links. ”',
+  'testimonials-tht-name': 'Myroslav',
+  'testimonials-tht-spec': 'Developer',
+
+  'testimonials-forth-txt': '“ TataRozhdez apps - is my first experience of online app rental. It allows you to buy a range of applications at a lower price than in the standard store. So far, all is satisfied - 10 out of 10! ”',
+  'testimonials-frt-name': 'Alexey',
+  'testimonials-frt-spec': 'Marketer',
+
+  'app-scr': 'TataRozhdez apps screenshoots',
+
+  'faq-title': 'FREQUENTLY ASKED QUESTIONS',
+  'fac-post-title': 'We have answered your questions',
+  'faq-first':  '1. Which countries are these extended to?',
+  'faq-second': '2. How long an application can be leased?',
+  'faq-third': '3. What is this for?',
+
+  'contact-title': 'Get in Touch',
+  'contact-first-name': 'First Name',
+  'contact-last-name': 'Last Name',
+  'contact-email': 'Email address',
+  'contact-subject': 'Subject',
+  'contact-message': 'Message',
+  'contact-submit': 'Submit',
+}
+
+const ukConst = {
+  'header-home': 'Головна',
+  'header-works': 'Як працює',
+  'header-special': 'Особливості',
+  'header-pricing': 'Прайс',
+  'header-review': 'Обзір',
+  'header-contact': 'Контакти',
+
+  'maincont-service': 'Сервіс для аренди додатків',
+  'maincont-created': 'Розроблено TataRozhdez Company',
+  'maincont-more': 'Деталі',
+
+  'howworks-title': 'Як це працює?',
+
+  'testimonials': 'Отзиви',
+  'testimonials-first-txt': '“ КРУТО - одне з найпопулярніших сервисів з отримання додатків. Для зручного перегляду товари розділені на групи: напрямки, категорії, тип покупки, тип платформи. Я в основному користувалася глобальним типом покупки. ”',
+  'testimonials-ft-name': 'Екатерина',
+  'testimonials-ft-spec': 'SEO-спеціаліст',
+
+  'testimonials-second-txt': '“ Я використовую його більше місяця, і ще не знайшов жодної вади. Це дуже зручно та інформативно. Рекомендую всім друзям та знайомим. ”',
+  'testimonials-sct-name': 'Роман',
+  'testimonials-sct-spec': 'Фрилансер',
+
+  'testimonials-third-txt': '“ Додаток рекомендували друзі. TataRozhdez apps - просто і легко зрозуміти. Послуга швидка, без зайвих проблем. Після початку оренди програми можна створити всі можливі посилання. ”',
+  'testimonials-tht-name': 'Мирослав',
+  'testimonials-tht-spec': 'Розробник',
+
+  'testimonials-forth-txt': '“ TataRozhdez apps - мій перший досвід онлайн-оренди додатків. Це гарна можливість купити ряд додатків за нижчою ціною, ніж в стандартному store. Поки всім задоволен - 10 з 10! ”',
+  'testimonials-frt-name': 'Олексій',
+  'testimonials-frt-spec': 'Маркетолог',
+
+  'app-scr': 'знімки екранів TataRozhdez apps',
+
+  'faq-title': 'ПОШИРЕНІ ЗАПИТАННЯ',
+  'fac-post-title': 'Ми відповіли на ваші найпоширеніші запитання',
+  'faq-first':  '1. На які країни це поширюється?',
+  'faq-second': '2. Як довго можна орендувати додаток?',
+  'faq-third': '3. Для чого це потрібно?',
+
+  'contact-title': 'Залишити заявку',
+  'contact-first-name': 'І\'я',
+  'contact-last-name': 'Прізвище',
+  'contact-email': 'Електронна пошта',
+  'contact-subject': 'Тема',
+  'contact-message': 'Повідомлення',
+  'contact-submit': 'Відправити',
+}
+
+const ruConst = {
+  'header-home': 'Главная',
+  'header-works': 'Как работает',
+  'header-special': 'Особенности',
+  'header-pricing': 'Прайс',
+  'header-review': 'Обзор',
+  'header-contact': 'Контакты',
+
+  'maincont-service': 'Сервис для аренды приложений',
+  'maincont-created': 'Разработано TataRozhdez Company',
+  'maincont-more': 'Детали',
+
+  'howworks-title': 'Как это работает?',
+
+  'testimonials': 'Отзывы',
+  'testimonials-first-txt': '“ КРУТО - одно из самых популярных сервисов по получению приложений. Для удобного просмотра товары разделены на группы: направления, категории, тип покупки, тип платформы. Я в основном пользовалась глобальным типом покупки. ”',
+  'testimonials-ft-name': 'Екатерина',
+  'testimonials-ft-spec': 'SEO-специалист',
+
+  'testimonials-second-txt': '“ Пользуюсь уже больше месяца и пока недостатков не обнаружил. Все очень удобно и информативно. Рекомендую всем знакомым и друзьям. ”',
+  'testimonials-sct-name': 'Роман',
+  'testimonials-sct-spec': 'Фрилансер',
+
+  'testimonials-third-txt': '“ Приложение рекомендовали коллеги. TataRozhdez apps - просто и понятно. Обслуживание быстрое, без проблем. После начала аренды приложения можно сгенерировать всевозможные ссылки. ”',
+  'testimonials-tht-name': 'Мирослав',
+  'testimonials-tht-spec': 'ИТ-Разработчик',
+
+  'testimonials-forth-txt': '“ TataRozhdez apps - мой первый опыт онлайн-аренды приложений. Это хорошая возможность купить ряд приложений по более низкой цене, чем в стандартном store. Пока всем доволен - 10 из 10! ”',
+  'testimonials-frt-name': 'Алексей',
+  'testimonials-frt-spec': 'Маркетолог',
+
+  'app-scr': 'снимки экранов TataRozhdez apps',
+
+  'faq-title': 'ЧАСТО ЗАДАВАЕМЫЕ ВОПРОСЫ',
+  'fac-post-title': 'Мы ответили на ваши часто задаваемые вопросы',
+  'faq-first':  '1. На какие страны это распространяется?',
+  'faq-second': '2. Как долго действует аренда?',
+  'faq-third': '3. Для чего это нужно?',
+
+  'contact-title': 'Оставить заявку',
+  'contact-first-name': 'Имя',
+  'contact-last-name': 'Фамилия',
+  'contact-email': 'Електронная почта',
+  'contact-subject': 'Тема',
+  'contact-message': 'Сообщение',
+  'contact-submit': 'Отправить',
+
+}
+
+jQuery(function ($, locale) {
+
+  $.i18n().load({
+    en: enConst,
+    uk: ukConst,
+    ru: ruConst
+  })
+
+  $('#switch-locale').on('click', 'a', function (e) {
+    e.preventDefault()
+
+    const data = $(this).data('locale')
+    $.i18n({locale: data})
+
+    $('html').i18n()
+  })
+
+  $('html').i18n()
+})
+
+
+
